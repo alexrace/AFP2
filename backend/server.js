830130fded3f8b2937ae9d8d3e0ea62049d48d25
vereczkibalint <vereczkibalint@gmail.com>
2020-03-09 14:03:57 +0100

    changed path to api calls

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,23 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const app = express()
-const port = process.env.PORT || 5000
+const userRouter = require('./routes/userRouter');
+const productRouter = require('./routes/productRouter');
+const partRouter = require('./routes/partRouter');
 
-app.use(bodyParser.json())
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send("It's working!")
-})
+});
+
+app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
+app.use('/api/parts', partRouter);
 
 app.listen(port, () => {
-    console.log(`A szerver elindult a http://localhost:${port}/ címen!`)
-})
+    console.log(`A szerver elindult a http://localhost:${port}/ címen!`);
+});
