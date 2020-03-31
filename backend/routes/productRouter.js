57eecaf6fceec6e73473f4dfd4137f1191774162
vereczkibalint <vereczkibalint@gmail.com>
2020-03-31 14:52:35 +0200

    created basic router for products listing

--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -7,13 +7,13 @@ const productService = require('../services/productService');
 router.get('/', (req, res) => {
     productService.fetchProducts({}, (products) => {
         res.status(200).send(products);
-    })
+    });
 });
 
 router.get('/:product_id', (req, res) => {
     productService.fetchProducts({product_id: req.params['product_id']}, (product) => {
         res.status(200).send(product);
-    })
+    });
 });
 
 module.exports = router;
