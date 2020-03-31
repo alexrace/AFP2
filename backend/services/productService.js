42426887f4cf6c025d25ccf35418aa6e3ccd945b
vereczkibalint <vereczkibalint@gmail.com>
2020-03-31 14:05:01 +0200

    created service for products

---
+++ backend/services/productService.js
@@ -0,0 +1,17 @@
+const productDAO = require('../dao/productDAO');
+
+class productService{
+    fetchProducts(queryOptions, callback){
+        productDAO.read((products) => {
+            if(queryOptions['product_id'] != undefined){
+                callback(products.filter((product) => {
+                    return product.id == queryOptions['product_id'];
+                }));
+            }else{
+                callback(products);
+            }
+        });
+    }
+}
+
+module.exports = new productService();
