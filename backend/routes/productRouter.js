e34859660f726f9dc484fa38172129ed726b74db
vereczkibalint <vereczkibalint@gmail.com>
2020-04-05 21:02:54 +0200

    #6 created all routes

--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -2,18 +2,16 @@ const express = require('express');
 
 const router = express.Router();
 
-const productService = require('../services/productService');
+const productController = require('../controllers/productController');
 
-router.get('/', (req, res) => {
-    productService.fetchProducts({}, (products) => {
-        res.status(200).send(products);
-    });
-});
+router.get('/', productController.fetchAll);
 
-router.get('/:product_id', (req, res) => {
-    productService.fetchProducts({product_id: req.params['product_id']}, (product) => {
-        res.status(200).send(product);
-    });
-});
+router.get('/:product_id', productController.fetchOne);
+
+router.post('/create', productController.createProduct);
+
+router.patch('/:product_id', productController.updateProduct);
+
+router.delete('/delete/:product_id', productController.deleteProduct);
 
 module.exports = router;
