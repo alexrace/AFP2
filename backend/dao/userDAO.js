c540c269884fb20263d2a80d3f5a763f6bc6799c
vereczkibalint <vereczkibalint@gmail.com>
2020-04-25 17:52:42 +0200

    #8 - user auth and register with bcrypt implemented

--- backend/dao/userDAO.js
+++ backend/dao/userDAO.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql');
 const dbConfig = require('../db/db.config');
+const bcrypt = require('bcrypt');
 
 class userDAO{
     constructor(){
@@ -16,13 +17,20 @@ class userDAO{
     }
 
     create(user, success, error){
-        this.connection.query("INSERT INTO users SET ?", user, (err, result) => {
-           if(err){
-               return error(err);
-           }else{
-               return success();
-           }
-        });
+        bcrypt.hash(user.password, 10, (err, hash) => {
+            if(err) error(err);
+            else{
+                user.password = hash;
+                this.connection.query("INSERT INTO users SET ?", user, (err, result) => {
+                    if(err){
+                        return error(err);
+                    }else{
+                        return success();
+                    }
+                });
+            }
+        })
+
     }
 
     read(callback){
@@ -52,7 +60,34 @@ class userDAO{
             {
                 success();
             }
-        })
+        });
+    }
+
+    authenticate(userdata, error, success){
+        this.connection.query("SELECT u.user_id, u.username, u.email, u.password, s.site_name FROM users u INNER JOIN sites s ON u.user_site=s.site_id WHERE username = ?", [userdata.username], (err, result) => {
+           if(err) error(err.message);
+           else{
+               if(result.length > 0){
+                    bcrypt.compare(userdata.password, result[0].password, (err, hashResult) => {
+                        if(err) error(err);
+                        if(hashResult){
+                            const user =
+                                {
+                                    user_id: result[0].user_id,
+                                    username: result[0].username,
+                                    email: result[0].email,
+                                    user_site: result[0].site_name
+                                };
+                            success(user);
+                        }else{
+                            error('Sikertelen bejelentkezés!');
+                        }
+                    });
+               }else{
+                   error('Nem létező felhasználó!');
+               }
+           }
+        });
     }
 }
 
