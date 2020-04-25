928541fdc4f7379a1c353ba4dc1fd8d19e340f69
vereczkibalint <vereczkibalint@gmail.com>
2020-04-25 11:38:14 +0200

    #7 - implemented part order

--- backend/dao/partDAO.js
+++ backend/dao/partDAO.js
@@ -60,6 +60,22 @@ class partDAO{
             }
         })
     }
+
+    order(part_id, part_qty, success, error){
+        this.connection.query("SELECT inv.inventory_id FROM inventory inv INNER JOIN sites s ON inv.site_id=s.site_id WHERE s.site_name='Local worker'", {}, (err, result) => {
+            if(err) error(err.message);
+            else{
+                let inv_id = result[0].inventory_id;
+
+                this.connection.query("INSERT INTO parts_inventory (inventory_id, part_id, qty, availability) VALUES(?,?,?,?)",[inv_id, part_id, part_qty,'available'], (err, result) => {
+                   if(err) error(err.message);
+                   else{
+                       success();
+                   }
+                });
+            }
+        });
+    }
 }
 
 module.exports = new partDAO();
