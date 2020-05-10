d6e16d98ef5ae11204f32c5d0e166e44529038e9
vereczkibalint <vereczkibalint@gmail.com>
2020-05-10 20:53:40 +0200

    navbar component created for navigation on the site

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+    Link
+} from "react-router-dom";
+
+class NavBar extends React.Component{
+    render(){
+        return(
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <Link className="navbar-brand" to="/">Inventory Manager System</Link>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item active">
+                                <Link className="nav-link" to="/search">Keresés</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/inventory">Raktár</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>    
+        );
+    }
+}
+
+export default NavBar;
