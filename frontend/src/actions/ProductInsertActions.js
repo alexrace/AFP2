8cf7f74f73d94f111fe2a12bce0f188b3a0d0d03
vereczkibalint <vereczkibalint@gmail.com>
2020-05-11 14:29:09 +0200

    changed product_qty to description

--- frontend/src/actions/ProductInsertActions.js
+++ frontend/src/actions/ProductInsertActions.js
@@ -2,14 +2,14 @@ import dispatcher from '../AppDispatcher';
 
 class ProductInsertActions{
     
-    insert(product_id, product_name,product_price,product_qty){
+    insert(product_id, product_name,product_price,description){
         dispatcher.handleViewAction({
             actionType : 'PRODUCT_INSERT',
             payload : {
                 product_id : product_id,
                 product_name : product_name,
                 product_price : product_price,
-                product_qty : product_qty
+                description : description
                 
             }
         });
