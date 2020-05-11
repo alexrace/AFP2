508850310249594d5d25622cbdd4bacc4a212653
vereczkibalint <vereczkibalint@gmail.com>
2020-05-11 14:29:56 +0200

    added style and changed qty to description

--- frontend/src/components/ProductInsert.js
+++ frontend/src/components/ProductInsert.js
@@ -8,69 +8,71 @@ class ProductInsert extends React.Component{
             product_id : "",
             product_name : "",
             product_price : "",
-            product_qty : ""
+            description : ""
         }
     }
 
     render(){
         return(
-            <div>
-                <table>
-                    <tr>
-                        <td>Id</td>
-                        <td><input 
-                                type={"number"}
-                                value={this.state.product_id}
-                                onChange={(e)=>{
-                                    let st = this.state;
-                                    st.product_id = e.target.value;
-                                    this.setState(st);}}
+            <div className="insertForm mx-auto mt-5">
+                <div className="table-responsive">
+                    <table class="table borderless">
+                        <tr>
+                            <td>Id</td>
+                            <td><input 
+                                    type={"number"}
+                                    value={this.state.product_id}
+                                    onChange={(e)=>{
+                                        let st = this.state;
+                                        st.product_id = e.target.value;
+                                        this.setState(st);}}
+                                /></td>
+                        </tr>
+                        <tr>
+                            <td>Name</td>
+                            <td><input type={"text"}
+                                        value={this.state.product_name}
+                                        onChange={(e)=>{
+                                            let st = this.state;
+                                            st.product_name = e.target.value;
+                                            this.setState(st);
+                                        }}           
                             /></td>
-                    </tr>
-                    <tr>
-                        <td>Name</td>
-                        <td><input type={"text"}
-                                    value={this.state.product_name}
-                                    onChange={(e)=>{
-                                        let st = this.state;
-                                        st.product_name = e.target.value;
-                                        this.setState(st);
-                                    }}           
-                        /></td>
-                    </tr>
-                    <tr>
-                        <td>Price</td>
-                        <td><input type={"number"}
-                                    value={this.state.product_price}
-                                    onChange={(e)=>{
-                                        let st = this.state;
-                                        st.product_price = e.target.value;
-                                        this.setState(st);
-                                    }}           
-                        /></td>
-                    </tr>
-                    <tr>
-                        <td>Piece</td>
-                        <td><input type={"number"}
-                                    value={this.state.product_qty}
-                                    onChange={(e)=>{
-                                        let st = this.state;
-                                        st.product_qty = e.target.value;
-                                        this.setState(st);
-                                    }}           
-                        /></td>
-                    </tr>
-                    <tr>
-                        <td 
-                         colSpan={2}>
-                            <button
-                                 className="btn btn-info"
-                                 onClick={()=>{ProductInsertActions.insert(this.state.product_id, this.state.product_name,this.state.product_price,this.state.product_qty)}}
-                                >Insert
-                            </button>
-                        </td>
-                    </tr>
-                </table>
+                        </tr>
+                        <tr>
+                            <td>Price</td>
+                            <td><input type={"number"}
+                                        value={this.state.product_price}
+                                        onChange={(e)=>{
+                                            let st = this.state;
+                                            st.product_price = e.target.value;
+                                            this.setState(st);
+                                        }}           
+                            /></td>
+                        </tr>
+                        <tr>
+                            <td>Description</td>
+                            <td><input type={"text"}
+                                        value={this.state.description}
+                                        onChange={(e)=>{
+                                            let st = this.state;
+                                            st.description = e.target.value;
+                                            this.setState(st);
+                                        }}           
+                            /></td>
+                        </tr>
+                        <tr>
+                            <td 
+                            colSpan={2}>
+                                <button
+                                    className="btn btn-info"
+                                    onClick={()=>{ProductInsertActions.insert(this.state.product_id, this.state.product_name,this.state.product_price,this.state.description)}}
+                                    >Insert
+                                </button>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
             </div>
         );
     }
