910ad83b557279912482b970949b975f42b70077
vereczkibalint <vereczkibalint@gmail.com>
2020-05-11 14:30:59 +0200

    added product add route and component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/dist/jquery.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
 import NavBar from "./components/NavBar";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
 import Search from "./components/Search";
+import ProductInsert from './components/ProductInsert';
 
 function App() {
   
@@ -12,6 +18,7 @@ function App() {
           <Switch>
             <Route path="/search" exact component={Search} />
             <Route path="/inventory" />
+            <Route path="/products/add" exact component={ProductInsert} />
           </Switch>
         </Router>
     );
