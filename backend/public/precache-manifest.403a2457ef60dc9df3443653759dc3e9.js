e39ace23843fd295405806f363b1ff82556f4ca6
vereczkibalint <vereczkibalint@gmail.com>
2020-05-12 20:14:09 +0200

    builded react put into public folder

---
+++ backend/public/precache-manifest.403a2457ef60dc9df3443653759dc3e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd7206ab4abf49b96152b6249825121a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22237ec425927d03abe6",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "03e5e4679d4275e15a0b",
+    "url": "/static/css/main.a59d0cc4.chunk.css"
+  },
+  {
+    "revision": "22237ec425927d03abe6",
+    "url": "/static/js/2.1fa6c71a.chunk.js"
+  },
+  {
+    "revision": "aa23c390fb3a5a2b569b0aa36373a600",
+    "url": "/static/js/2.1fa6c71a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03e5e4679d4275e15a0b",
+    "url": "/static/js/main.8ddcec98.chunk.js"
+  },
+  {
+    "revision": "b05506d192fa13a4b14c",
+    "url": "/static/js/runtime-main.cb08f1e8.js"
+  }
+]);
